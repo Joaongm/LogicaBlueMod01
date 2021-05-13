44edba88d1820dab5a2cc9eaaa3437d9de6821b5
João <hlabeats@gmail.com>
2021-05-13 12:36:59 -0300

    primeira

---
+++ Funcoes.py
@@ -0,0 +1,25 @@
+print('Aula de Funções')
+
+# definindo funções:#
+def blue():
+    print('Eu sou bluemer')
+
+# chamando uma função: ##
+
+blue()    
+print()
+
+print('Exemplo de função com parâmetros:')
+# definindo a função #
+
+print()
+def subtracao(a,b):
+    print('O resultado da subtração é:' , a-b)
+
+    #chamando a função #
+subtracao(50, 3)
+print()
+def func_a(a, b):
+    print( (a + b)**2(b/a)))
+a=int(input('Valor a:'))
+b=int(input('Valor b: '))
